7d30c3de181c7d70a8947438c588998fd4f3cff4
Jasper Van Proeyen <trianglejuice@mac.com>
2015-05-13 10:02:12 +0200

    * Removed redundant code * Prepared for the last (hard) step

--- src/scripts/directives/stickygetreal.js
+++ src/scripts/directives/stickygetreal.js
@@ -21,6 +21,10 @@
 }]).factory('fixedCont',['$window','$timeout',function($window,$timeout){
 
   var padding;
+
+  /*
+  Trigger update function while scrolling
+   */
   $timeout(function(){
     padding = parseInt($('body').css('padding-top'));
 
@@ -40,115 +44,54 @@
     calculateValues();
   },250);
 
-  function getScrollTop() {
-    var prop = 'pageYOffset',
-      method = 'scrollTop';
-    return $window ? (prop in $window) ? $window[ prop ] :
-      $window.document.documentElement[ method ] :
-      $window.document.body[ method ];
-  }
-
-
-
   var components=[];
   var stickyList=[];
-  var currentSticky={dummy:null,original:null};
 
-  function updateOld(){
-    var scrollTop = getScrollTop();
-    $('#valueTop').html(scrollTop);
-    $('#elementPos').html(components[0].top);
+  /*
+  Loop through this while scrolling
+   */
+  function update(){
+    var scrollTop = getScrollTop();//console.log(scrollTop)
     var lengthC = components.length;
 
-    if(currentSticky.dummy){
-      if(scrollTop+padding <= currentSticky.dummy.position().top){
-        removeSticky();
-      }
-    }
-
-    components.forEach(function(value,key){
-      var next = key+1 < lengthC;
-      var element = $(value.elem);
-      if(next && scrollTop >= value.top && scrollTop < value.stop){
-
-        var nextElement = $(components[key+1].elem);
-        if((nextElement.position().top - scrollTop) <= element.outerHeight(true)+padding){
-          var verschil = (nextElement.position().top-padding-scrollTop);
-          // console.log(element.outerHeight(true)); 40
-          var secondTop = (padding+verschil); // top van rode blok
-          var firstTop = (padding+verschil-element.outerHeight(true)); // top van gele blok
-          // console.log(verschil+element.outerHeight(true));
-          setSticky(value,firstTop); 
-          element.css('top',(firstTop)+'px');
-        }else{
-          setSticky(value,padding);
-          element.css('top',padding+'px');  
-        }
-        //
-      }else if(!next && scrollTop >= value.top){
-        setSticky(value,padding);  
-      }
-    });
-
-  }
-
-  function update(){
-     var scrollTop = getScrollTop();console.log(scrollTop)
-     var lengthC = components.length;
+    // Go through list of all components
+    // value = scrollTop of current object
     components.forEach(function(value,key){
       if(value.extra){
         scrollTop += value.extra;
       }
-       //We have a next element.
+
+      // Check if we have a next/previous element
       var next = key+1 < lengthC;
       var prev = key-1 > -1;
-      var element = $(value.elem);
+      var element = $(value.elem); // current
 
-      //If not in viewport, next element.
+      // If not in viewport, go to next element
       if(scrollTop > value.top+element.outerHeight(true) && scrollTop>value.stop){
         removeSticky(value);
         return;
       }
 
-     
+      // If there's a previous component of the same level:
+      // Add ((Height of prev element) - (height of elements on a higher level)) to current scrollTop
+      if(prev && components[key-1].level === value.level){
+        scrollTop+=$(components[key-1].elem).outerHeight(true)-components[key-1].extra;
+      }
 
-      //If we have a next element and the next element is on the SAME level.
-      //if(next){
-
-        /*
-          Hier moeten we enkel de hoogte van het huidige element weten als we het sticky zetten en die hoogte in het dummy element steken.
-          Die extra hoogte is ook belangrijk in combinatie met de positie van het volgende element (van hetzelfde level).
-        */
-        if(prev && components[key-1].level === value.level){
-          scrollTop+=$(components[key-1].elem).outerHeight(true)-components[key-1].extra;
-       //console.log('prev',$(value.elem).get(0).style.background)
-        }
-
-        if(next && components[key+1].level === value.level){
-          //console.log('next',$(value.elem).get(0).style.background)
-          //value.stop -=6;
-        }
-        /*
-          Hier moeten we ook de hoogte weten van de elementen van een hoger level die reeds sticky staan (kunnen meerdere levels zijn).
-          Voor de rest hebben die dezelfde behaviour als het eerste level
-        */
-       // console.log(scrollTop,value.top,value.stop,value.elem,scrollTop >= value.top && scrollTop < value.stop)
-       //console.log('top:'+value.top,'extra:'+value.extra,'stop:'+value.stop,'scroll:'+scrollTop,$(value.elem).get(0).style.background,scrollTop >= value.top && scrollTop < value.stop)
-        if(scrollTop >= value.top && scrollTop < value.stop){
-
-          addSticky(value,(prev && components[key-1].level === value.level)|| (next && components[key+1].level > value.level));
-        }else{
-          removeSticky(value);
-        }
-
-      //Set the element sticky if it reached the top
-     // } else {
-        //last element
-     // }
+      // If (viewport + sticky elements on same level) > scrollTop >= current element's top
+      // Make sticky (or remove sticky)
+      if(scrollTop >= value.top && scrollTop < value.stop){
+        addSticky(value);
+      }else{
+        removeSticky(value);
+      }
     });
   }
 
-  function addSticky(obj,bool){
+  /*
+  Make element sticky
+   */
+  function addSticky(obj){
     if(obj && obj.elem){
       var elem = $(obj.elem);
       if(isSticky(obj)===-1){
@@ -157,21 +100,22 @@
           topHeight+= obj.extra;
         }
         obj.dummy = createDummy(elem);
-        if(!bool){
-          elem.after(obj.dummy);
-        }        
+        elem.after(obj.dummy);
         makeSticky(elem,topHeight);
         stickyList.push(obj);
       }
     }
   }
 
+  /*
+  Remove sticky
+   */
   function removeSticky(obj){
     if(obj && obj.elem){
       var elem = $(obj.elem);
       var stickyIndex = isSticky(obj);
       if(stickyIndex > -1){
-        
+
         var sticky = stickyList[stickyIndex];
         sticky.dummy.remove();
         $(sticky.elem).removeClass('sticky');
@@ -181,37 +125,18 @@
     }
   }
 
+  /*
+  Dummy element to prevent content from 'jumping'
+   */
   function createDummy(elem){
     elem = $(elem);
     return $( '<div>' ).height(elem.outerHeight(true));
   }
 
-  function makeSticky(elem,top){
-    elem = $(elem);
-    elem.addClass('sticky');
-    elem.css('top',top+'px');
-  }
-
-  function setSticky(obj,margin){
-    if((currentSticky.dummy && $(currentSticky.original.elem).get(0) !== $(obj.elem).get(0)) || !currentSticky.dummy){
-      removeSticky();
-      var element = $(obj.elem);
-      currentSticky.dummy = $( '<div>' ).height(element.outerHeight(true));
-      element.after(currentSticky.dummy);
-      element.addClass('sticky');
-      element.css('top',margin+'px');
-      currentSticky.original = obj;
-    }    
-  }
-
-  /*function removeSticky(){
-    if(currentSticky.dummy){
-      currentSticky.dummy.remove();
-      $(currentSticky.original.elem).removeClass('sticky');
-      currentSticky = {};
-    }
-  }*/
-
+  /*
+  Runs on startup to calculate all necessary values
+  To do: make it also run on viewport resize
+   */
   function calculateValues(){
     var lengthC = components.length;
     components.forEach(function(value,key){
@@ -224,13 +149,13 @@
             value.stop = components[i].top-element.outerHeight(true);
             break;
           }else if(components[i].level < value.level && value.stop === undefined){
-            value.stop = components[i].top;                        
+            value.stop = components[i].top;
           }
         }
         //console.log($window.innerHeight,$window,($(document).height() - $(window).height()))
         if(value.stop === undefined){
           value.stop = $(document).height() - $(window).height();
-        }   
+        }
       }
       for(var j=key;j>=0;j--){
               if(components[j].level < value.level){
@@ -241,6 +166,9 @@
     });
   }
 
+  /*
+  Checks if element is sticky and if yes, at which position
+   */
   function isSticky(obj){
     var index = -1;
     for (var i = stickyList.length; i--;) {
@@ -253,7 +181,30 @@
     return index;
   }
 
+  /*
+  Helper function to get scrollTop
+   */
+  function getScrollTop() {
+    var prop = 'pageYOffset',
+      method = 'scrollTop';
+    return $window ? (prop in $window) ? $window[ prop ] :
+      $window.document.documentElement[ method ] :
+      $window.document.body[ method ];
+  }
+
+  /*
+  Help function make sticky
+   */
+  function makeSticky(elem,top){
+    elem = $(elem);
+    elem.addClass('sticky');
+    elem.css('top',top+'px');
+  }
+
+
+  // Create scope for factory
   var ctrl = {};
+
   ctrl.register= function(element,level){
     var nakedEl = $(element).get(0);
       components.push({elem: $(element),top:$(element).position().top,level:level});
@@ -263,7 +214,8 @@
           return a - b;
       });
   }
-  
+
   return ctrl;
+
 }]);
 })();
